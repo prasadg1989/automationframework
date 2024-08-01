b971b86d6909f35640700e898cae213594487492
Prasad <prasad26.te@gmail.com>
2024-08-01 16:02:35 +0530

    Updated Takes Screenshot

---
+++ src/main/java/com/java/framework/TestListener.java
@@ -0,0 +1,13 @@
+package com.java.framework;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+public class TestListener extends Utils implements ITestListener{
+
+	public void onTestFailure(ITestResult result) {
+	
+		
+
+	}
+	
+}
